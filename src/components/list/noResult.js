8c1b35fb671cb11be2f2fc3e5d471b1201716467
Franco Borrelli <francoborrelli96@outlook.com>
2019-02-21 00:49:58 -0300

    Change noResults message

--- src/components/list/noResult.js
+++ src/components/list/noResult.js
@@ -2,4 +2,4 @@ import React from "react";
 
 import Card from "../card/card";
 
-export default () => <Card style={{ width: 300 }}>Agrega un To Do</Card>;
+export default () => <Card>Todavia no tienes tareas en tu lista</Card>;
