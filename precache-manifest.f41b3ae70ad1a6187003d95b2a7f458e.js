c649f98186e37d408f1764bf61c182f5c066d883
Franco Borrelli <francoborrelli96@outlook.com>
2019-02-23 15:25:09 -0300

    Updates

---
+++ precache-manifest.f41b3ae70ad1a6187003d95b2a7f458e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a87775f5415b47ec7ede",
+    "url": "/todolist/static/js/runtime~main.a87775f5.js"
+  },
+  {
+    "revision": "b144f8e27e6b1db921ce",
+    "url": "/todolist/static/js/main.b144f8e2.chunk.js"
+  },
+  {
+    "revision": "7891176f93ae5551fb70",
+    "url": "/todolist/static/js/2.7891176f.chunk.js"
+  },
+  {
+    "revision": "b144f8e27e6b1db921ce",
+    "url": "/todolist/static/css/main.82a2a8f2.chunk.css"
+  },
+  {
+    "revision": "7891176f93ae5551fb70",
+    "url": "/todolist/static/css/2.7887ff34.chunk.css"
+  },
+  {
+    "revision": "564ea7cf1e64684adc0a72ad7ccbb9a4",
+    "url": "/todolist/index.html"
+  }
+];
